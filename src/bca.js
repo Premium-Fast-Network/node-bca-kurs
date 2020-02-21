f093e56e3e618fc0da7ac98311f5a7ea5ac66257
JuniYadi <juniyadi94@gmail.com>
2020-02-21 23:46:06 +0700

    Added BCA Class

---
+++ src/bca.js
@@ -0,0 +1,61 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+const cheerioTableparser = require('cheerio-tableparser')
+
+class BCA {
+    constructor(url) {
+        this.url = 'https://www.bca.co.id/Individu/Sarana/Kurs-dan-Suku-Bunga/Kurs-dan-Kalkulator'
+
+        if (url) {
+            this.url = url
+        }
+    }
+
+    getCurrency() {
+        return new Promise((resolve, reject) => {
+            axios.get(this.url)
+                .then((res) => {
+
+                    // loaded result to cheerio
+                    const $ = cheerio.load(res.data)
+
+                    // loaded cheerio to tableparser
+                    cheerioTableparser($)
+                    const html = $('.table').parsetable(true, true, true);
+
+                    // parse date
+                    const checkDate = html[1][0]
+                    const dateAndTime = checkDate.replace('e-Rate', '')
+                    const dateOnly = dateAndTime.replace(' / ', ' ')
+
+                    // make variable for save kurs
+                    const dataKurs = {}
+
+                    for (let i = 0; i < html[0].length; i++) {
+
+                        // check if data isset
+                        if (html[0][i + 2]) {
+
+                            const currency = html[0][i + 2]
+                            const buy = html[1][i + 2]
+                            const sell = html[2][i + 2]
+
+                            dataKurs[currency] = { buy, sell }
+                        }
+                    }
+
+                    const result = {
+                        check: dateOnly,
+                        data: dataKurs
+                    }
+
+                    resolve(result)
+                })
+                .catch((err) => {
+                    reject(err)
+                })
+        })
+    }
+}
+
+module.exports = BCA
